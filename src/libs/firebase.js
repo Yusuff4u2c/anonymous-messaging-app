8a6363722cd7bfee01539f404ed155f14678c2cd
cremirdevio <josephajibodu@gmail.com>
2023-09-28 12:50:39 +0100

    env file added

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAu-Mr9QE52PG6bq6xVGTz82cwssx8S2kM",
-  authDomain: "anonymous-messaging-app-d29ce.firebaseapp.com",
-  projectId: "anonymous-messaging-app-d29ce",
-  storageBucket: "anonymous-messaging-app-d29ce.appspot.com",
-  messagingSenderId: "97635209224",
-  appId: "1:97635209224:web:0d25ae4e2bc0d2e24b41bd",
-  measurementId: "G-X5GBFS61DM"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.AUTHDOMAIN,
+  projectId: import.meta.env.PROJECTID,
+  storageBucket: import.meta.env.STORAGEBUCKET,
+  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
+  appId: import.meta.env.APPID,
+  measurementId: import.meta.env.MEASUREMENTID
 };
 
 // Initialize Firebase
